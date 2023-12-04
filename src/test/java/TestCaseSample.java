65e5eec5f00a3b367e85b5d10c9d972085ddfd48
DarshitKansara96 <darshitkansara96@gmail.com>
2023-12-04 18:44:55 +0530

    excel commit

---
+++ src/test/java/TestCaseSample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TestCaseSample {
+	
+	public static void main(String[] args) throws Exception {
+		
+		DataDriven d = new DataDriven();
+		
+		ArrayList data = d.getData("AddProfile" , "TestData");
+		
+		System.out.println(data);
+	}
+
+}
